0456f18ff8931010d0a4278967d45e2ddca1b0c1
Your Name <you@example.com>
2023-11-28 19:47:22 -0600

    update Git 3

--- hello git 3.py
+++ hello git 3.py
@@ -1,1 +1,11 @@
 print ("hello git 3")
+print ("siguiente paso, ir a chalco ")
+print ("contactar grua")
+print ("contactar mecanico")
+print ("contactar hojalatero")
+print ("ir por el auto a zumpango")
+
+
+
+
+
